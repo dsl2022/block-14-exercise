d5471e54260cbc4711cf7f8059663e40908bf1c7
dsl2022 <2015rpro@gmail.com>
2023-04-28 22:57:17 -0400

    updated

--- loop-exe-script.js
+++ loop-exe-script.js
@@ -1,26 +1,48 @@
 // Write a for loop that prints the numbers 1 through 10 to the console.
-
+for (let i = 0; i <= 10; i++){
+    console.log(i)
+}
 // Write a for loop that prints the even numbers from 2 to 20 to the console.
+for (let i = 0; i <= 20; i++){
+    if(i%2===0)console.log(i)
+}
 
 // Write a while loop that counts down from 10 to 1 and prints the numbers to the console.
-
-// Write a while loop that prompts the user to enter a number greater than 5. Keep prompting until the user enters a valid number.
+let i = 10
+while(i>=1){
+    console.log(i)
+    i--
+}
 
 
 // Write a for loop that prints the following pattern to the console:
-// *
-// **
+// *   
+// **   
 // ***
 // ****
 // *****
 
+for (let i = 1; i <= 5; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+
 // Write a for loop that prints the following pattern to the console:
 // *****
 // ****
 // ***
 // **
 // *
-
+for (let i = 5; i >= 1; i--) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
 
 // bonus 
 // Write a while loop that generates random numbers between 1 and 10 until a number greater than 8 is generated. The loop should print each generated number to the console.
--- spread-rest-opt.js
+++ spread-rest-opt.js
@@ -1,19 +1,29 @@
 // Write a function that takes two arrays and returns a new array that contains all the elements of both arrays, using the spread operator.
 
 function concatArrays(arr1, arr2){
-    // TODO, write your code here
+    // TODO, write your code here    
+    return [...arr1,...arr2]
 }
 
-console.log(concatArrays([1, 2, 3], [4, 5, 6])); // Output: [1, 2, 3, 4, 5, 6]
-console.log(concatArrays(['apple', 'banana'], ['cherry', 'dragonfruit'])); // Output: ['apple', 'banana', 'cherry', 'dragonfruit']
+// console.log(concatArrays([1, 2, 3], [4, 5, 6])); // Output: [1, 2, 3, 4, 5, 6]
+// console.log(concatArrays(['apple', 'banana'], ['cherry', 'dragonfruit'])); // Output: ['apple', 'banana', 'cherry', 'dragonfruit']
 
 
-// Write a function that takes an array and returns a new array with the first and last elements removed, using the rest operator.
+// Write a function that takes an array and returns a new array with the first and last elements removed
 
-function removeFirstAndLast(){
-
+function removeFirstAndLast([first,...arr]){
+    return arr.slice(1,-1)
 }
+
+function removeFirstAndLast(arr){
+    return arr.slice(1,-1)
+}
+// function removeFirstAndLast(arr){
+//     console.log("19",arr.slice(0,-1))
+//     const [,...newArray] = arr.slice(0,-1)
+//     return newArray
+//     }
 
 console.log(removeFirstAndLast([1, 2, 3, 4])); // Output: [2, 3]
-console.log(removeFirstAndLast(['apple', 'banana', 'cherry', 'dragonfruit'])); // Output: ['banana', 'cherry']
+// console.log(removeFirstAndLast(['apple', 'banana', 'cherry', 'dragonfruit'])); // Output: ['banana', 'cherry']
 
